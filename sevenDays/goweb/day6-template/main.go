ebcc461756adb6509c45b6d48fceb4c3b9530a35
ucdo <phpsarc@gmail.com>
2024-03-25 22:05:23 +0800

    finished goweb-7

--- sevenDays/goweb/day6-template/main.go
+++ sevenDays/goweb/day6-template/main.go
@@ -10,8 +10,13 @@ func main() {
 	r.Use(gee.Logger())
 	r.Use(gee.Recovery())
 
-	r.GET("/panic", func(ctx *gee.Context) {
+	r.GET("/", func(ctx *gee.Context) {
 		ctx.String(http.StatusOK, "Hello geek\n")
+	})
+
+	r.GET("/panic", func(ctx *gee.Context) {
+		names := []string{"geektutu"}
+		ctx.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":8989")
